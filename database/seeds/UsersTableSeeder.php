0d9569b53833a74fc041b1fa721e3f20c3779827
Hayk <php-user@users.noreply.github.com>
2019-04-21 17:24:35 -0400

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,10 +12,10 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-        	'name' => 'John Smith',
-            'email' => 'smith@test.com',
-        	'address' => 'Some address here',
-        	'password' => '$2y$10$OJsadONpbRzd5FNPiqDbJulHMvCwRZdFQakCVYTiVUnBYv1R8ATO.',
+        	'name' => 'Anna Smith',
+            'email' => 'anna@test.com',
+            'address' => 'Some address here',
+            'password' => Hash::make('annaanna'),
         	'created_at' => date('Y-m-d H:m:s'),
         	'updated_at' => date('Y-m-d H:m:s')
         ]);
